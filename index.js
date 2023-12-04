a7ce6de90d0b2c07fae0c5596b37ad3127de0c5f
Zachary Elbring <elbringzach@gmail.com>
2023-12-04 13:16:22 -0600

    Added questions to be prompted to the user.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+const promptQuestions = [
+    'Provide 3 letters you wish to include in your logo.',
+
+    'Pick a shape for your logo.',
+
+    'Pick a color for your shape.',
+    
+    'Pick a color for your 3 letters.',
+];
