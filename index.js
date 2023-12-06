9c0e341d4feef9d2964f4dc39555054365b37714
Zachary Elbring <elbringzach@gmail.com>
2023-12-05 20:41:46 -0600

    Completed index.js with an ifelse statement and a writefile. Also updated shapes to look better.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Rectangle = require('./lib/rectangle');
 const Circle = require('./lib/circle');
 const Triangle = require('./lib/triangle');
 var userShape = '';
+const file = './Examples/logo.svg';
 
 const promptQuestions = [
     'Provide 3 letters you wish to include in your logo.',
@@ -49,7 +50,14 @@ function startShape() {
         } else if(content.shape == 'Circle') {
             userShape = new Circle(content.text, content.colorText, content.colorShape);
         } else {
-            console.log('Error creating your logo. Please try again.')
+            console.log('Error creating your logo. Please try again.');
         }
+
+        userShape.generateShape();
+        const userShapeCode = userShape.generate();
+        fs.writeFile(file, userShapeCode, (err) => {
+            err ? console.log(err) : console.log('Your logo has been created successfully!');
+        })
     })
 }
+startShape();
