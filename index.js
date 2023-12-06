3f575d2c504b33b47161086b587b0c21dfc734fd
Zachary Elbring <elbringzach@gmail.com>
2023-12-05 20:16:17 -0600

    Added required files and startShape function to index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Shape = require('./lib/shape');
+const Rectangle = require('./lib/rectangle');
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+var userShape = '';
 
 const promptQuestions = [
     'Provide 3 letters you wish to include in your logo.',
@@ -10,3 +15,41 @@ const promptQuestions = [
     
     'Pick a color for your 3 letters.',
 ];
+
+function startShape() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: promptQuestions[0]
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: promptQuestions[1],
+            choices: ['Triangle', 'Rectangle', 'Circle']
+        },
+        {
+            type: 'input',
+            name: 'colorShape',
+            message: promptQuestions[2]
+        },
+        {
+            type: 'input',
+            name: 'colorText',
+            message: promptQuestions[3]
+        }
+    ])
+    .then((content) => {
+        if(content.shape == 'Triangle') {
+            userShape = new Triangle(content.text, content.colorText, content.colorShape);
+        } else if(content.shape == 'Rectangle') {
+            userShape = new Rectangle(content.text, content.colorText, content.colorShape);
+        } else if(content.shape == 'Circle') {
+            userShape = new Circle(content.text, content.colorText, content.colorShape);
+        } else {
+            console.log('Error creating your logo. Please try again.')
+        }
+    })
+}
